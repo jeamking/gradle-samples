5c0fb2c7530c61f0572c155000836d36406a4ef7
jeamking <jinjian10@126.com>
2018-12-31 21:11:34 +0800

    初始化

---
+++ springbootMultiModule/search/src/test/java/com/demo/UserTest.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class UserTest {
+
+	@Test
+	public void testGetUser() {
+		Assert.assertEquals("s1", "s1");
+	}
+
+}
