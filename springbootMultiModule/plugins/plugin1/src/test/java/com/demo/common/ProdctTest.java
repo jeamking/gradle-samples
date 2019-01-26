f0643904aa2eeccbdb1bebd4b1559897f8b707f1
jeamking <jinjian10@126.com>
2019-01-26 22:06:05 +0800

    无

---
+++ springbootMultiModule/plugins/plugin1/src/test/java/com/demo/common/ProdctTest.java
@@ -0,0 +1,13 @@
+package com.demo.common;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ProdctTest {
+
+	@Test
+	public void testGetProduct() {
+		Assert.assertEquals("s1", "s1");
+	}
+
+}
